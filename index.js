dc287cfde8661e433d57bbc04ad3f62b1a3c293a
Linter <github@saharsh.tech>
2020-07-19 22:12:42 +0000

    🚨Prettier: Fix code style issues

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const args = process.argv.slice(2);
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const targetLink = 'https://lukec.me';
+const targetLink = "https://lukec.me";
 
 const shortenURL = async (longLink) => {
   try {
     const response = await fetch(`https://kutt.it/api/v2/links`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         target: longLink,
         reuse: true,
-        domain: 'https://go.srsh.link/',
+        domain: "https://go.srsh.link/",
       }),
       headers: {
-        'content-type': 'application/json',
-        'X-API-KEY': process.env.KEY,
+        "content-type": "application/json",
+        "X-API-KEY": process.env.KEY,
       },
     });
     const shortLink = await response.json();
