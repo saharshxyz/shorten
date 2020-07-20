718b1d625cff6ef60b8558b9d56db3b68e664761
Saharsh Yeruva <hello@saharsh.tech>
2020-07-20 04:09:25 -0400

    🐛fix: checks for http at the start of clipboard Preiously, the script would make sure that the clipboard had http or https somewhere in it, but now it will only create a short link if http is at the very beggining of the clipboard

--- index.js
+++ index.js
@@ -22,20 +22,21 @@ const shortenURL = async (longLink) => {
     const shortLinkData = await response.json();
     let shortLink = shortLinkData.link;
 
-    if (shortLink.includes("http", 0)) {
-      shortLink = shortLink.replace(/http/, "https");
-    }
-
+    shortLink = shortLink.replace(/http/, "https");
     clipboardy.writeSync(shortLink);
   } catch (error) {
     console.error(error);
   }
 };
 
-const targetLink = clipboardy.readSync();
-
-if (targetLink.includes("http", 0) || targetLink.includes("https", 0)) {
-  shortenURL(targetLink);
-} else {
-  clipboardy.writeSync("Not a valid link. Make sure it has http or https.");
+const run = (longLink) => {
+  const startsWIthHTTP = /^http/m;
+  if (startsWIthHTTP.exec(longLink) !== null) {
+    shortenURL(longLink);
+  } else {
+    clipboardy.writeSync("Not a valid link. Make sure it has http or https.");
+  }
 }
+
+const targetLink = clipboardy.readSync();
+run(targetLink);
