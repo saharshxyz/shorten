5a350eac193202036c5c2c20db18f37dafe7a6e0
Harrison Katz <github@hkatz.dev>
2020-07-22 20:34:44 -0700

    Fix http -> https
    Also tab formatting

--- index.js
+++ index.js
@@ -5,39 +5,41 @@ const applescript = require("applescript");
 require("dotenv").config();
 
 const shorten = async (url, subDomain) => {
-  try {
-    const response = await fetch("https://kutt.it/api/v2/links", {
-      method: "POST",
-      body: JSON.stringify({
-        target: url.toString(),
-        reuse: true,
-        domain: `https://${subDomain}.srsh.link/`,
-      }),
-      headers: {
-        "content-type": "application/json",
-        "X-API-KEY": process.env.KEY,
-      },
-    });
-    return (await response.json()).link;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await fetch("https://kutt.it/api/v2/links", {
+			method: "POST",
+			body: JSON.stringify({
+				target: url.toString(),
+				reuse: true,
+				domain: `https://${subDomain}.srsh.link/`,
+			}),
+			headers: {
+				"content-type": "application/json",
+				"X-API-KEY": process.env.KEY,
+			},
+		});
+		return (await response.json()).link;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 const shortenURL = async url => {
-	const shortLink = await shorten(url, url.hostname === "cln.sh" ? "sc" : "go")
-	clipboardy.writeSync(shortLink);
-  return shortLink;
+	let shortLink = new URL(await shorten(url, url.hostname === "cln.sh" ? "sc" : "go"));
+	if (shortLink.protocol === "http:") shortLink.protocol = "https:";
+	const shortUrl = shortLink.toString();
+	clipboardy.writeSync(shortUrl);
+	return shortUrl;
 };
 
 const run = async longLink => {
-  if (!longLink.match(/^https?:\/\//)) longLink = `http://${longLink}`;
+	if (!longLink.match(/^https?:\/\//)) longLink = `http://${longLink}`;
 	let notification;
 	try {
 		const url = new URL(longLink);
 		notification = `display notification "${longLink}" with title "Link Shortened" subtitle "${await shortenURL(url)}" sound name "purr"`;
 	} catch (error) {
-    notification = `display notification "${longLink}" with title "Invalid Link" subtitle "Make sure link has http or https" sound name "glass"`;
+		notification = `display notification "${longLink}" with title "Invalid Link" subtitle "Make sure link has http or https" sound name "glass"`;
 	}
 	applescript.execString(notification);
 };
